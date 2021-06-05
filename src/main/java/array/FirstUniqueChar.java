31341f34da5a4a5d4096dfbdd76c0364ccb97bab
Rituraj Pandey <riturajpandey01@gmail.com>
2021-06-05 17:50:01 +0530

    adding codes for week 2

--- src/main/java/array/FirstUniqueChar.java
+++ src/main/java/array/FirstUniqueChar.java
@@ -1,10 +1,32 @@
 package array;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/first-unique-character-in-a-string/
  */
+
 public class FirstUniqueChar {
     static int firstUniqueChar(String s) {
-        return Integer.MIN_VALUE; // YOUR CODE HERE
+    	HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
+        char[] arr=s.toCharArray();
+        for(char c : arr)
+        {
+            if(freqMap.containsKey(c))
+                freqMap.put(c,freqMap.get(c)+1);
+            else
+                freqMap.put(c,1);
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(freqMap.get(s.charAt(i))==1)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String ar[]) {
+    	Scanner S=new Scanner(System.in);
+    	System.out.println("Enter a string");
+    	String s=S.next();
+    	System.out.println("First unique occurance is at position "+ firstUniqueChar(s));
     }
 }
--- src/main/java/array/ValidAnagram.java
+++ src/main/java/array/ValidAnagram.java
@@ -1,10 +1,43 @@
 package array;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/valid-anagram/
  */
+
 public class ValidAnagram {
-    static boolean isAnagram(String s, String t) {
-        return false; // YOUR CODE HERE
+	static boolean isAnagram(String s, String t) {
+    	HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
+        char[] sa=s.toCharArray();
+        char[] ta=t.toCharArray();
+        if(s.length()!=t.length())
+            return false;
+        for(char c : sa) {
+            if(freqMap.containsKey(c))
+                freqMap.put(c,freqMap.get(c)+1);
+            else
+                freqMap.put(c,1);
+        }
+        for(char c : ta) {
+            if(freqMap.containsKey(c))
+                freqMap.put(c,freqMap.get(c)-1);
+            else
+                freqMap.put(c,1);
+        }
+        for(int i=0;i<s.length();i++) {
+            if(freqMap.get(s.charAt(i))>0)
+                return false;
+        }
+        return true;
     }
+	public static void main(String ar[]) {
+		Scanner S=new Scanner(System.in);
+		System.out.println("Enter two strings");
+		String s=S.next();
+		String t=S.next();
+		if(isAnagram(s,t))
+			System.out.println("Anagrams!");
+		else
+			System.out.println("Not Anagrams.");
+	}
 }
